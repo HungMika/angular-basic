a40a50dc099f5bb8139191810063e5458bcf7fdc
Hung Dang <106718735+HungMika@users.noreply.github.com>
2025-06-27 11:40:04 +0700

    ng-basic: format .prettierrc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderLayoutComponent } from './header-layout/header-layout.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderLayoutComponent],
+  imports: [RouterOutlet, HeaderLayoutComponent, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   //example of a text title object
   title = {
-    name: 'Angular App',
+    name: 'Angular Application',
     version: '1.0.0',
-    description: 'A simple Angular application'
+    description: 'A simple Angular application',
   };
 
   // example of a boolean flag
   isDisable = true;
 
   // example of a atributes
-  ImageContent = "test image";
+  ImageContent = 'test image';
+
+  //example of an event handler
+  btnName = 'Click Me';
+  message = 'Hello, Angular!';
+  onButtonClick(): void {
+    this.message = 'Button was clicked!';
+    this.btnName = 'Clicked!';
+  }
+
+  updateField(): void {
+    console.log('Field updated!');
+  }
+
+  // example of a method to 2-way bind a value
+  bindValue = '';
 }
